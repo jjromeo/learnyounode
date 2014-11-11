206ac333854bbbb82c9b3eedfe65736110b063ba
Henry Stanley <henry@henrystanley.com>
2014-11-11 12:49:33 +0000

    Removed dead code

--- moduleSix.js
+++ moduleSix.js
@@ -14,23 +14,5 @@ function listFiles(dir, ext, callback) {
     })
 }
 
-
-
-
-
-
-// function listFiles (dir, filter, callback) {
-//     fs.readdir(process.argv[2], function(err, list) {
-//         list.sort();
-//         var array = new Array;
-//         for(var i = 0; i < list.length; i++) {
-//             if(path.extname(list[i]) == "." + filter) {
-//                 array.push(list[i]);
-//             }
-//         };  
-//         callback(array);
-//     });
-// }
-
 module.exports = listFiles
 
