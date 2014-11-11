565420abb60c9c4150f402c9a174bebbc07b9c28
Henry Stanley <henry@henrystanley.com>
2014-11-11 12:47:41 +0000

    Completed exercise six

--- 06-make-it-modular.js
+++ 06-make-it-modular.js
@@ -1,9 +1,12 @@
 var mymodule = require('./moduleSix');
 var fs = require('fs');
 var path = require('path');
+
 var ext = process.argv[3];
 var dir = process.argv[2];
 
 mymodule(dir, ext, function(err, data){
-    console.log(data)
+    for (var i = 0; i < data.length; i++) {
+      console.log(data[i]);
+    };
 });
--- moduleSix.js
+++ moduleSix.js
@@ -1,16 +1,36 @@
 var fs = require('fs');
 var path = require('path');
-function listFiles (dir, filter, callback) {
-    fs.readdir(process.argv[2], function(err, list) {
-        list.sort();
+
+function listFiles(dir, ext, callback) {
+    fs.readdir(dir, function(err, list) {
+        if (err) return callback(err);
         var array = new Array;
         for(var i = 0; i < list.length; i++) {
-            if(path.extname(list[i]) == "." + ) {
+            if(path.extname(list[i]) == "." + ext) {
                 array.push(list[i]);
             }
-        };  
-        callback(array);
-    });
+        };
+        callback(null, array);
+    })
 }
+
+
+
+
+
+
+// function listFiles (dir, filter, callback) {
+//     fs.readdir(process.argv[2], function(err, list) {
+//         list.sort();
+//         var array = new Array;
+//         for(var i = 0; i < list.length; i++) {
+//             if(path.extname(list[i]) == "." + filter) {
+//                 array.push(list[i]);
+//             }
+//         };  
+//         callback(array);
+//     });
+// }
+
 module.exports = listFiles
 
